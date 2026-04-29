b46f808eebd0197bab61980d39fe93bdafe1bcbd
dikoananda2905-byte <alfandy516@users.noreply.github.com>
2026-04-29 08:35:46 +0700

    update kembali seperti sebelumnya

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename)
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ADMIN-DASHBOARD/", // gihub-pages
+//   base: "/ADMIN-DASHBOARD/", // gihub-pages
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
